d95b206fbdfef9b70be2994d11a2d9869f4c6aa2
satoshi <satoshidat@gmail.com>
2017-06-02 14:54:58 +0900

    プロジェクト登録

---
+++ if_rensyu1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace if_rensyu1
+{
+    public partial class Form1 : Form
+       
+    {
+        int vx = 10;
+        int vy = 10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            button1.Left-= vx;
+
+
+            if (button1.Left < 0)
+            {
+                vx += 10;
+                button1.Left += vx;
+               
+            }
+            else if (button1.Left > 300)
+           
+            button1.Left -= vx;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            
+            
+        }
+    }
+}
